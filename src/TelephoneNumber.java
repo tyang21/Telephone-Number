144ca2524826bdeffb1e5ec0ead5b8d2a03bded2
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-10-03 18:04:41 -0400

    Add files via upload

---
+++ src/TelephoneNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TelephoneNumber {
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter Your Name: "); //Display Prompt
+		String name = in.next();
+		in.nextLine();
+		System.out.println("Enter your Phone Number: "); //Display Prompt
+		String phone = in.next();
+		in.nextLine();
+		
+		String first = phone.substring(0, 3);
+		String second = phone.substring(3,6);
+		String last = phone.substring(6,10);
+		
+		System.out.printf("Name: %-20s", name );
+		System.out.print("(" + first + ")" + second + "-" + last);
+		in.close();
+		
+
+	}
+
+}
